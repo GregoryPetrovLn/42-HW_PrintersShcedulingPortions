991a1e5f2cdd7b9b12f26a198f48c618981e09ae
Gregory <grishap96123@gmail.com>
2020-11-15 14:07:49 +0200

    final

--- src/Printer.java
+++ src/Printer.java
@@ -1,10 +1,38 @@
-public class Printer extends Thread{
+public class Printer extends Thread {
+    private int num;
+    private int nPortions;
+    private int sizePortion;
+    private int id;
+    private Printer next;
 
+    public void setNext(Printer next) {
+        this.next = next;
+    }
 
-
+    public Printer(int num, int nPortions, int id, Printer next) {
+        this.num = num;
+        this.nPortions = nPortions;
+        this.id = id;
+        this.next = next;
+        this.sizePortion = num/ nPortions;
+    }
 
     @Override
-    public void run(){
-
+    public void run() {
+        for (int i = 1; i <= nPortions; i++) {
+            startPrintCycle(i);
+        }
     }
+
+
+    private void startPrintCycle(int i) {
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            System.out.println((""  + id).repeat(sizePortion));
+
+            next.interrupt();
+        }
+    }
+
 }
--- src/PrinterController.java
+++ src/PrinterController.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
 public class PrinterController {
-    private final int N_PRINTERS = 4;
-    private final int N_NUMBERS = 100;
-    private final int N_PORTIONS = 10;
+    public static void main(String[] args) throws InterruptedException {
+        int N_PRINTERS = 4;
+        int N_NUMBERS = 100;
+        int N_PORTIONS = 10;
+
+        Printer printers[] = getPrinters(N_PRINTERS, N_PORTIONS, N_NUMBERS);
+
+       Arrays.stream(printers).forEach(Printer::start);
+       printers[0].interrupt();
+
+
+    }
+
+    private static Printer[] getPrinters(int nPrinters, int portions, int num) {
+        Printer[] printers = new Printer[nPrinters];
+        printers[nPrinters-1] = new Printer(num, portions, nPrinters-1, null);
+
+        for (int i = nPrinters-2; i >= 0; i--) {
+            printers[i] = new Printer(num,portions, i, printers[i+1]);
+        }
+
+        printers[nPrinters-1].setNext(printers[0]);
+
+        return printers;
+    }
+
+
 }
